0cb6afa896713b721153abb40ed1c6f3deca408c
Lizaveta <lizadondysh@icloud.com>
2024-01-28 16:13:41 +0200

    feat: add solution for fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { dirname, join } from 'path';
+import { access, writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const newFilePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
+  try {
+    await access(newFilePath);
+    throw new Error(ERROR_MESSAGE);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await writeFile(newFilePath, FILE_CONTENT);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
