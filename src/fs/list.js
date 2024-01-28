89977baccd1c31aadcfb3792384e2ef87bcc163c
Lizaveta <lizadondysh@icloud.com>
2024-01-28 18:19:03 +0200

    feat: add solution for fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { dirname, join } from 'path';
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+import doesFileExist from '../utils/doesFileExist.mjs';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, FOLDER_NAME);
+  const doesFolderExist = await doesFileExist(folderPath);
+
+  if (!doesFolderExist) {
+    throw new Error(ERROR_MESSAGE);
+  }
+
+  const fileNames = await readdir(folderPath);
+  console.log(fileNames);
 };
 
 await list();
