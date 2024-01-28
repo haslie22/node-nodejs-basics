a34a69abdaedeb169131a25940e0d4d4e0c17a6e
Lizaveta <lizadondysh@icloud.com>
2024-01-28 17:48:23 +0200

    feat: add solution for fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import { dirname, join } from 'path';
+import { rm } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+import doesFileExist from '../utils/doesFileExist.mjs';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToRemove.txt';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+  const isFilePresent = await doesFileExist(filePath);
+
+  if (!isFilePresent) {
+    throw new Error(ERROR_MESSAGE);
+  }
+
+  await rm(filePath);
 };
 
 await remove();
