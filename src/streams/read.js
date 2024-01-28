3f52a4f080829f1b7c504b6f68c303623f02c104
Lizaveta <lizadondysh@icloud.com>
2024-01-29 01:53:11 +0200

    feat: add solution for streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+  const input = createReadStream(filePath, 'utf-8');
+
+  input.pipe(process.stdout);
 };
 
 await read();
