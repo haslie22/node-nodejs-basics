6e8f9ba344f27c3d478cbef5084b70ecece5b0b6
Lizaveta <lizadondysh@icloud.com>
2024-01-29 20:59:34 +0200

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { fork, spawn } from 'child_process';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'script.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childPath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
+  const childPr = fork(childPath, args, {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+
+  childPr.stdout.pipe(process.stdout);
+  process.stdin.pipe(childPr.stdin);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['avocado', 'corn', 'tomato', 'carrot', 'eggplant']);
